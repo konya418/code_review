6f58625c62b9abdbed4c5f128ffc3a4bc643e85c
konya418 <98363134+konya418@users.noreply.github.com>
2022-03-30 20:39:05 -0400

    added comments inline

--- phackeR/R/place_holder.R
+++ phackeR/R/place_holder.R
@@ -1,7 +1,11 @@
+#COMMENT: all scripts should generally start off with a comment describing the purpose, author(s), and any additional notes regarding structure/libraries
+
+#COMMENT: libraries used were not included in the beginning. Include GGPLOT2 library since it is being heavily utilized in this script
+
 place_holder <- function(){
   ggplot(x = seq(0, 1, .1), y = seq(0, 1, .1)) + 
     geom_vline(xintercept = .05, linetype = 'dashed') + 
-    scale_x_continuous(breaks=seq(0,1,.05), limits = c(-.01, 1.01), expand = expansion()) + 
+    scale_x_continuous(breaks=seq(0,1,.05), limits = c(-.01, 1.01), expand = expansion()) + #The expansion function comes from ggplot2- the library should be noted  
     scale_y_continuous(labels = NULL, breaks = NULL) + 
     labs(y = "", x = 'P value', 
          title = 'Collect Data to See Results!') + 
